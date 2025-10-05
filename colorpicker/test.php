fdd6da663e82968a56ab83f0d6b8cdbb6afbd776
mbilalawan <mbilalawan@gmail.com>
2025-10-05 13:03:11 +0300

    Delete colorpicker directory

--- colorpicker/test.php
+++
@@ -1,4 +0,0 @@
-<!--For color picker-->
-<script type="text/javascript" src="js/jscolor.js"></script>
-
-<input class="color {hash:true}" value="<?php if(stripslashes($content_details_row['statements_color'])=='') echo "#FFFFFF";else echo stripslashes($content_details_row['statements_color']);?>" name="statements_color">
